3cd88cb0fb1d85a77f3cf61e75be18ae39047332
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-01 20:18:21 +0200

    Add styles

--- src/components/scores.jsx
+++ src/components/scores.jsx
@@ -2,11 +2,9 @@ import "/src/styles/scores.css";
 
 export default function Score({ score, highScore }) {
   return (
-    <div className="scores-container">
-      <div className="scores">
-        <div className="current-score">Current Score: {score}</div>
-        <div className="high-score">High Score: {highScore}</div>
-      </div>
+    <div className="scores">
+      <div className="current-score">Current Score: {score}</div>
+      <div className="high-score">High Score: {highScore}</div>
     </div>
   );
 }
