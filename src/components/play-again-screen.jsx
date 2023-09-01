e6f8e58651da216f8ef9278444d52704930a03c7
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-01 14:19:04 +0200

    Replace modal with play again screen component

---
+++ src/components/play-again-screen.jsx
@@ -0,0 +1,9 @@
+export default function PlayAgainScreen() {
+  return (
+    <div className="modal">
+      <div className="play-again-btn">
+        <button>Play Again?</button>
+      </div>
+    </div>
+  );
+}
